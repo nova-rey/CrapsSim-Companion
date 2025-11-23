8c82408803bd35253a74a01bf032e0a890c7da34
Nova - Rey <nova.rey4200@gmail.com>
2025-11-22 22:38:17 -0600

    Add CrapsSim Engine API runner

---
+++ node-red-contrib-craps/api-config/api-config.js
@@ -0,0 +1,15 @@
+module.exports = function(RED) {
+    function CrapsApiConfigNode(config) {
+        RED.nodes.createNode(this, config);
+        this.base_url = config.base_url || "http://127.0.0.1:8000";
+        this.profile_id = config.profile_id || "default";
+        this.default_seed_mode = config.default_seed_mode || "random";
+        this.seed = config.seed;
+        this.timeout_ms = config.timeout_ms || 5000;
+        this.retries = config.retries || 0;
+        this.retry_backoff_ms = config.retry_backoff_ms || 0;
+        this.auth_token = config.auth_token;
+    }
+
+    RED.nodes.registerType("craps-api-config", CrapsApiConfigNode);
+};
